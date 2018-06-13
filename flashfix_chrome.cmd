7300636bcafd469286f7211db3f7fcd3385d59a2
klabeh <28295828+klabeh@users.noreply.github.com>
2018-06-13 13:09:09 +0200

    Create flashfix_chrome.cmd

---
+++ flashfix_chrome.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+mkdir -p %LOCALAPPDATA%\Google\Chrome\User Data\Default\Pepper Data\Shockwave Flash\System
+echo EventJitterMicroseconds=1 >> %LOCALAPPDATA%\Google\Chrome\User Data\Default\Pepper Data\Shockwave Flash\System\mms.cfg
+echo TimerJitterMicroseconds=1 >> %LOCALAPPDATA%\Google\Chrome\User Data\Default\Pepper Data\Shockwave Flash\System\mms.cfg
+
+echo FERTIG! DONE IN!
+
+Pause
