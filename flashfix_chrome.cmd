6e57370004acaa61a23b41c4265e8d854d4e41b5
klabeh <28295828+klabeh@users.noreply.github.com>
2018-06-15 14:40:01 +0200

    Update flashfix_chrome.cmd

--- flashfix_chrome.cmd
+++ flashfix_chrome.cmd
@@ -1,8 +1,8 @@
 @echo off
 
 mkdir "%LOCALAPPDATA%\Google\Chrome\User Data\Default\Pepper Data\Shockwave Flash\System"
-echo EventJitterMicroseconds=1 >> %LOCALAPPDATA%\Google\Chrome\User Data\Default\Pepper Data\Shockwave Flash\System\mms.cfg
-echo TimerJitterMicroseconds=1 >> %LOCALAPPDATA%\Google\Chrome\User Data\Default\Pepper Data\Shockwave Flash\System\mms.cfg
+echo EventJitterMicroseconds=1 >> "%LOCALAPPDATA%\Google\Chrome\User Data\Default\Pepper Data\Shockwave Flash\System\mms.cfg"
+echo TimerJitterMicroseconds=1 >> "%LOCALAPPDATA%\Google\Chrome\User Data\Default\Pepper Data\Shockwave Flash\System\mms.cfg"
 
 echo FERTIG! DONE IN!
 
